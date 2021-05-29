eb203a92a1bd713fa355fc4d007e3fcf8b127825
Tanya Engelhart <tkengelhart@gmail.com>
2021-05-28 23:10:35 -0500

    updated routes functions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,20 @@
 import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import bootstrap from 'bootstrap'
-import MovieList from '../MovieList/MovieList'
+import bootstrap from 'bootstrap';
+import MovieList from '../MovieList/MovieList';
+import Details from '../Details/Details';
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
+      <h1>The Movies Saga</h1>
       <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-
+        <Route path="/details" exact>
+          <Details />
+        </Route>
         {/* Details page */}
 
         {/* Add Movie page */}
