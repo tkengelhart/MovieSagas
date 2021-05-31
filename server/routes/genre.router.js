c1852df9c382138a54c7ddb4724c47a34879bd20
Tanya Engelhart <tkengelhart@gmail.com>
2021-05-31 00:51:03 -0500

    updated query

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  const queryText = `SELECT * FROM "genres" ORDER BY "name" ASC`;
+  const queryText = `SELECT * FROM "genres" ORDER BY "id" ASC`;
   pool.query(queryText)
     .then(result => {
       res.send(result.rows);
